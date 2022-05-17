a277f8baf78d60972331374b77f99b51852a3df5
estelle <1395288376@qq.com>
2022-05-17 11:51:32 +0800

    二叉树遍历迭代方法

--- 树/简单/144.二叉树的前序遍历.js
+++ 树/简单/144.二叉树的前序遍历.js
@@ -17,6 +17,8 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+// 递归
+// 按照访问根节点——左子树——右子树的方式遍历这棵树，而在访问左子树或者右子树的时候我们按照同样的方式遍历，直到遍历完整棵树
 var preorderTraversal = function(root) {
     let arr = []
     if(!root) return arr
@@ -25,5 +27,23 @@ var preorderTraversal = function(root) {
     arr.push(...preorderTraversal(root.right))
     return arr
 };
-// @lc code=end
 
+// 迭代
+var preorderTraversal = function(root){
+    const res = []
+    // 保存当前的节点
+    const stk = []
+    while(root || stk.length){
+        // 不断往左子树走，将当前节点的值保存起来，然后将当前节点保存在栈中
+        while(root){
+            stk.push(root)
+            res.push(root.val)
+            root = root.left
+        }
+        // 当前节点为空，说明左边走到头了，从栈中弹出最后一个节点
+        root=stk.pop()
+        // 然后走当前节点的右子树
+        root = root.right
+    }
+    return res
+}
--- 树/简单/145.二叉树的后序遍历.js
+++ 树/简单/145.二叉树的后序遍历.js
@@ -17,6 +17,8 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+// 递归
+// 按照访问左子树——右子树——根节点的方式遍历这棵树，而在访问左子树或者右子树的时候我们按照同样的方式遍历，直到遍历完整棵树
 var postorderTraversal = function(root) {
     let arr = []
     if(!root) return arr
@@ -25,5 +27,25 @@ var postorderTraversal = function(root) {
     arr.push(root.val)
     return arr
 };
-// @lc code=end
+
+// 迭代
+var postorderTraversal = function(root){
+    const res = []
+    // 保存当前的节点
+    const stk = []
+    while(root || stk.length){
+        while(root){
+            stk.push(root)
+            // 将当前节点的值保存起来
+            res.push(root.val)
+            // 然后不停的往右子树走，直到走到底
+            root = root.right
+        }
+        // 右子树走到底之后就把最后保存的节点弹出来，开始走左子树
+        root = stk.pop()
+        root = root.left
+    }
+    // 通过以上操作得到的结果是根节点-右节点-左节点，将其反转过来即是后序遍历的值
+    return res.reverse()
+}
 
